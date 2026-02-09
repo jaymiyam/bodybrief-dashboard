ec0bf0ac6cdb91426126c71f6fd23abd0b83b651
Jaymi Yam <jaymiyam@gmail.com>
2026-02-09 23:06:03 +0800

    jest-dom testing setup

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,22 @@
-// import { describe, it, expect } from 'vitest';
-// import { render, screen } from '@testing-library/react';
-// import { userEvent } from '@testing-library/user-event';
-// import App from '../App';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import App from '../App';
 
-// describe('user interactions with About Modal', () => {
+describe('user interactions with About Modal', () => {
+  it('opens the dialog when user clicks info button', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+    const infoButton = screen.getByText(/about this project/i);
+    expect(infoButton).toBeInTheDocument();
+    await user.click(infoButton);
+    const modal = screen.getByRole('dialog');
+    expect(modal).toBeVisible();
+  });
 
-// });
+  it('closes the dialog when user clicks the cross icon', () => {
+    render(<App />);
+  });
+
+  it('closes the dialog when user clicks the close button', () => {});
+});
