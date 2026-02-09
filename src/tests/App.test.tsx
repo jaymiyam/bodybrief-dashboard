af40ee7045185f74d71d16172402234f85bd03b6
Jaymi Yam <jaymiyam@gmail.com>
2026-02-09 23:25:43 +0800

    added modal visibility testing

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -6,17 +6,42 @@ import App from '../App';
 describe('user interactions with About Modal', () => {
   it('opens the dialog when user clicks info button', async () => {
     const user = userEvent.setup();
+
     render(<App />);
+
     const infoButton = screen.getByText(/about this project/i);
     expect(infoButton).toBeInTheDocument();
+
     await user.click(infoButton);
     const modal = screen.getByRole('dialog');
     expect(modal).toBeVisible();
   });
 
-  it('closes the dialog when user clicks the cross icon', () => {
+  it('closes the dialog when user clicks the cross icon', async () => {
+    const user = userEvent.setup();
+
     render(<App />);
+    const infoButton = screen.getByText(/about this project/i);
+    await user.click(infoButton);
+    const modal = screen.getByRole('dialog');
+    expect(modal).toBeVisible();
+
+    const crossIcon = screen.getByLabelText(/Close dialog/i);
+    await user.click(crossIcon);
+    expect(modal).not.toBeVisible();
   });
 
-  it('closes the dialog when user clicks the close button', () => {});
+  it('closes the dialog when user clicks the close button', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const infoButton = screen.getByText(/about this project/i);
+    await user.click(infoButton);
+    const modal = screen.getByRole('dialog');
+    expect(modal).toBeVisible();
+
+    const closeButton = screen.getByText(/Close/i);
+    await user.click(closeButton);
+    expect(modal).not.toBeVisible();
+  });
 });
